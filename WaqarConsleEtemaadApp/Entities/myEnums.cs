c6928d8af1bdd2a01768cac7eb7bc998705c8473
Junaid Tanvir <Junaid.tanvir@outlook.com>
2019-07-24 19:18:35 +0500

    Login + Customer Menu with Product Items

---
+++ WaqarConsleEtemaadApp/Entities/myEnums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaqarConsleEtemaadApp.Entities
+{
+
+    public enum RoleType
+    {
+        Customer = 1,
+        SalesMan,
+        MarketingManager,
+        SalesManager,
+        HRManager,
+        Owener
+    }
+}
--- WaqarConsleEtemaadApp/Program.cs
+++ WaqarConsleEtemaadApp/Program.cs
@@ -19,16 +19,67 @@ namespace WaqarConsleEtemaadApp
         {
             Program program = new Program();
 
-            program.LoginView();
+
+            program.UserType();
+
+        
 
             Console.ReadLine();
         }
 
 
 
-        public  void LoginView()
+        public void UserType()
         {
-            Console.WriteLine("\t\tWelcome To Etemaad Store");
+
+
+            Console.WriteLine("Please Select Your User Type \n\n\n" +
+                              "Press 1 for Admin\n" +
+                              "Press 2 for Customer\n"
+
+            );
+
+
+
+            var Usertype = int.Parse(Console.ReadLine());
+
+
+
+
+
+            switch (Usertype)
+            {
+
+                case 1: // Admin
+
+
+                    LoginView("Admin");
+
+                    break;
+
+                case 2: // Customer
+
+                    bool isUserExist = LoginView("Customer");
+
+                    if (isUserExist)
+                    {
+                        CustomerMenu();
+                    }
+
+                    break;
+
+            }
+
+
+
+
+        }
+
+
+        public  bool LoginView(string roleName)
+        {
+            Console.Clear();
+            Console.WriteLine($"\n\t\t {roleName}, Welcome To Etemaad Store\n\n");
 
 
             Console.Write("Please Enter UserName :");
@@ -42,23 +93,141 @@ namespace WaqarConsleEtemaadApp
             List<Login> LoginList = loginService.SelectAll();
 
 
-            var isUserExist = LoginList.Any(x => x.UserName == UserName && x.Password == password);
+            var isUserExist = LoginList.Any(x => x.UserName.ToLower() == UserName.ToLower() && x.Password == password);
 
 
             if (isUserExist)
             {
                 Console.Clear();
                 Console.WriteLine("Welcome in Etemaad Store");
+                return true;
             }
             else
             {
                 Console.Clear();
                 Console.WriteLine(" Sorry!!! UserName & Password does not Exist !!!");
+                return false;
             }
 
         }
 
 
+        public void CustomerMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("\n\n\n Press 1 for Checking Items\n" +
+                              " Press 2 for Buying Items ");
+
+
+            var customerMenuOptions = int.Parse(Console.ReadLine());
+
+
+
+
+
+            switch (customerMenuOptions)
+            {
+
+                case 1: // Checking Items
+
+
+                    CheckItems();
+
+                    break;
+
+                case 2: // Buying Items
+
+
+                    BuyingItems();
+
+
+                    break;
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+
+        public void CheckItems()
+        {
+            Console.Clear();
+            string[] products = new string[4] {"Daal", "Cheeni", "Ata", "Dahi"};
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(products[i]);
+            }
+
+
+        }
+
+
+        public void BuyingItems()
+        {
+            Console.Clear();
+            string[] products = new string[4] { "Daal", "Cheeni", "Ata", "Dahi" };
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Press {i + 1 } for {products[i]}  " );
+            }
+
+
+            var SelcectProduct = int.Parse(Console.ReadLine());
+
+
+
+
+
+            switch (SelcectProduct)
+            {
+
+                case 1: // Daal
+
+                    Console.Clear();
+
+                    Console.WriteLine("You Selected Daal \n\n\n ");
+
+                    Console.WriteLine("How much quantity do you want to Buy : ");
+                    var quantity = int.Parse(Console.ReadLine());
+
+
+                    break;
+
+
+                case 2: // Cheeeni
+
+                    break;
+
+                case 3: // Ata
+
+                    break;
+
+                case 4: // Dahi
+
+                    break;
+
+               
+
+
+            }
+
+
+
+
+        }
+
+
+    
 
     }
 }
